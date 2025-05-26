56837425983c742bbd108ffcf562be73a5884798
Arthur Tainmont <53705599+artainmo@users.noreply.github.com>
2025-05-27 00:14:53 +0200

    Update transform.sh

--- obsidian-html/transform.sh
+++ obsidian-html/transform.sh
@@ -1,1 +1,4 @@
 # Use 'sed - i' to make specific changes in 'index.md' if necessary.
+
+sed -i '' 's/!\[\[Certificate_NSL_Recognised.pdf\]\]/<div class="pdf-container"><embed src="Certificate_NSL_Recognised.pdf" type="application\/pdf"><\/div>/g' index.md
+sed -i '' 's/!\[\[open_project_school19_manuscript.pdf\]\]/<div class="pdf-container"><embed src="open_project_school19_manuscript.pdf" type="application\/pdf"><\/div>/g' index.md
