2e6c26a743fc78ddc6300ab7ff40b8ee957c3b81
Arthur Tainmont <53705599+artainmo@users.noreply.github.com>
2026-01-09 16:03:53 +0100

    Update transform.sh

--- obsidian-html/transform.sh
+++ obsidian-html/transform.sh
@@ -3,3 +3,8 @@
 sed -i '' 's/!\[\[Certificate_NSL_Recognised.pdf\]\]/<div class="pdf-container"><embed src="Certificate_NSL_Recognised.pdf" type="application\/pdf"><\/div>/g' index.md
 sed -i '' 's/!\[\[open_project_school19_manuscript.pdf\]\]/<div class="pdf-container"><embed src="open_project_school19_manuscript.pdf" type="application\/pdf"><\/div>/g' index.md
 
+sed -i '' 's/!\[\[LOI SNICA Edouard.pdf\]\]/<embed src="LOI SNICA Edouard.pdf" type="application\/pdf">/g' index.md
+sed -i '' 's/!\[\[LOI SNICA Asma.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Asma.pdf" type="application\/pdf"><\/div>/g' index.md
+sed -i '' 's/!\[\[LOI SNICA Maximilien.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Maximilien.pdf" type="application\/pdf"><\/div>/g' index.md
+sed -i '' 's/!\[\[LOI SNICA Elias.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Elias.pdf" type="application\/pdf"><\/div>/g' index.md
+sed -i '' 's/!\[\[LOI SNICA Momo.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Momo.pdf" type="application\/pdf"><\/div>/g' index.md
