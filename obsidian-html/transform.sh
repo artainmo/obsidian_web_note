c8559c13b6923cc04505b4f68f5afb14c4be9e88
Arthur Tainmont <53705599+artainmo@users.noreply.github.com>
2026-01-09 16:08:28 +0100

    Update transform.sh

--- obsidian-html/transform.sh
+++ obsidian-html/transform.sh
@@ -7,4 +7,4 @@ sed -i '' 's/!\[\[LOI SNICA Edouard.pdf\]\]/<embed src="LOI SNICA Edouard.pdf" t
 sed -i '' 's/!\[\[LOI SNICA Asma.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Asma.pdf" type="application\/pdf"><\/div>/g' index.md
 sed -i '' 's/!\[\[LOI SNICA Maximilien.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Maximilien.pdf" type="application\/pdf"><\/div>/g' index.md
 sed -i '' 's/!\[\[LOI SNICA Elias.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Elias.pdf" type="application\/pdf"><\/div>/g' index.md
-sed -i '' 's/!\[\[LOI SNICA Momo.pdf\]\]/<div class="pdf-container"><embed src="LOI SNICA Momo.pdf" type="application\/pdf"><\/div>/g' index.md
+sed -i '' 's/!\[\[LOI SNICA Momo.pdf\]\]/<embed src="LOI SNICA Momo.pdf" type="application\/pdf">/g' index.md
